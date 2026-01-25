fe1116a4c25fe67e4139c4ab398f0b7eb98fd133
elvin-mark <munoz.vega.em@gmail.com>
2026-01-25 20:16:35 +0900

    chore: adding devops tools to debian script

--- debian/startup.sh
+++ debian/startup.sh
@@ -37,3 +37,12 @@ ln -sf /opt/nvim/bin/nvim /usr/local/bin/nvim
 mkdir -p $HOME/.config/nvim
 git clone https://github.com/LazyVim/starter.git $HOME/.config/nvim
 
+
+# === DevOps tools ===
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+mkdir -p ~/.local/bin
+mv kubectl ~/.local/bin
+
